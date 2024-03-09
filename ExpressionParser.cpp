2011cac23af8a7bfe92230dead8c302c7dc1dce5
Yeshwanth2003 <yeshwanth.rmm.8@gmail.com>
2024-03-09 16:46:25 +0530

    new

---
+++ ExpressionParser.cpp
@@ -0,0 +1,151 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class ArithemeticEvaluator
+{
+protected:
+     // this indicates priorities of two operators
+     map<char, int> operator_priority = {
+         {'-', 1},
+         {'+', 2},
+         {'/', 3},
+         {'*', 4},
+         {'^', 5},
+     };
+     // postfix inner evaluator or helper
+
+     void postFix_h(char i, stack<char> *stack, string *postfix)
+     {
+          int t = i - '0'; // an initial value out of i to check whether this i is an integer or not
+          if (t >= 0 && t <= 9)
+               *postfix += i; // if integer simply add it to the string
+          else
+          {
+               if (stack->empty()) // else check whether stack is empty (which ensures that ther is no precedence conflict)
+               {
+                    stack->push(i); // if empty just push
+               }
+               else
+               {
+                    // if not empty chech if top of stack has more more priority than the i, in which case lower priority cannot go over higher one,
+                    // in worst case there might be many higher priority stacked on and on
+                    // if higher priority is found on top of the stack then pop it out and add to string
+                    while (!stack->empty() && (operator_priority[i] < operator_priority[stack->top()]))
+                    {
+                         *postfix += stack->top();
+                         stack->pop();
+                    }
+                    // finally push i into stack if no conflict remains
+                    stack->push(i);
+               }
+          }
+     }
+     // function to create postfix
+     // main function of postfix
+     string postFix()
+     {
+          // to store postfix string
+          string postfix = "";
+          // stack to keep track of operators
+          stack<char> stack;
+          // to keep eye on ()
+          bool is_Sub_Exp = false;
+          // iterate over input
+          for (auto i : this->input)
+          {
+               // if sub expression is encountered
+               if (is_Sub_Exp || (i == '(' || i == ')'))
+               {
+                    // handle sub expression in separate case
+                    if (i == '(')
+                    {
+                         // initialy push open bracket '('
+                         stack.push(i);
+                         is_Sub_Exp = true;
+                    }
+                    else if (i == ')')
+                    {
+                         // if ')' close is encountered pop until '(' also pop '('
+                         while (!stack.empty() && stack.top() != '(')
+                         {
+                              postfix += stack.top();
+                              stack.pop();
+                         }
+                         stack.pop();
+                         is_Sub_Exp = false;
+                    }
+                    else
+                    {
+                         // push rest sub expression until ')'
+                         postFix_h(i, &stack, &postfix);
+                    }
+               }
+               else
+               {
+                    // in case of normal expression create postfix and maintain stack
+                    postFix_h(i, &stack, &postfix);
+               }
+          }
+          // after iterating over string if any operator is left in stack then pop until and add into postfix
+          while (!stack.empty())
+          {
+               postfix += stack.top();
+               stack.pop();
+          }
+          return postfix;
+     }
+     // Supporiting Function
+     int add(int a, int b)
+     {
+          return a + b;
+     }
+     int sub(int a, int b)
+     {
+          return a - b;
+     }
+     int mul(int a, int b)
+     {
+          return a * b;
+     }
+     int div(int a, int b)
+     {
+          return a / b;
+     }
+     // end of supporting functions
+     string input; // this will hold given input string
+public:
+     ArithemeticEvaluator(string a)
+     {
+          this->input = a;
+     }
+     // actual evaluator function
+     int evaluate()
+     {
+          string postfix = this->postFix();
+          // to maintain operands
+          stack<char> stack;
+          for (auto i : postfix)
+          {
+               int t = i - '0';
+               if (t >= 0 && t <= 9)
+               {
+                    stack.push(i);
+               }
+               else
+               {
+                    // continue poping operands and evaluating
+               }
+          }
+          cout << postfix;
+          return 0;
+     }
+};
+
+int main()
+{
+     ArithemeticEvaluator a("2*(5-3)+4/(8-6)");
+     int ans = a.evaluate();
+     cout << endl
+          << "Answer:- " << ans;
+}
---
+++ Tree.cpp
@@ -0,0 +1,199 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Tree
+{
+protected:
+     struct Node
+     {
+          int val;
+          Node *r;
+          Node *l;
+          Node(int a) : val(a), r(nullptr), l(nullptr) {}
+     };
+     void printCallee(Node *n)
+     {
+          if (!n)
+               return;
+          printCallee(n->l);
+          cout << n->val << " ";
+          printCallee(n->r);
+     }
+
+     // delete's helper's
+     Node *getGreatest(Node *s) // returns greatest pre/parent node
+     {
+          Node *preGreat = nullptr;
+          while (s->r)
+          {
+               preGreat = s;
+               s = s->r;
+          }
+          return preGreat;
+     }
+     // root node
+     Node *root;
+
+public:
+     Tree()
+     {
+          this->root = nullptr;
+     }
+     // insert function
+     void add(int a)
+     {
+          Node *n = new Node(a);
+          if (!root)
+          {
+               root = n;
+               return;
+          }
+          Node *curr = root;
+          while (true)
+          {
+               if (curr->val < a)
+               {
+                    if (!curr->r)
+                    {
+                         curr->r = n;
+                         break;
+                    }
+                    curr = curr->r;
+               }
+               else
+               {
+                    if (!curr->l)
+                    {
+                         curr->l = n;
+                         break;
+                    }
+                    curr = curr->l;
+               }
+          }
+     }
+     void print()
+     {
+          Node *curr = root;
+          printCallee(curr);
+     }
+     // delete function
+     void del(int key)
+     {
+          Node *pre;
+          Node *curr = root;
+          // this loop finds the node to be deleted and its pre/parent node
+          while (true)
+          {
+               if (!curr || curr->val == key)
+                    break;
+               // this here works to find pre
+               pre = curr;
+               if (key > curr->val)
+               {
+                    curr = curr->r;
+               }
+               else
+                    curr = curr->l;
+          }
+          // only if node is found and pre/parent exist delete can be performed
+          if (curr && pre)
+          {
+               // if left 0 , if right 1
+               int indiactor;
+               // check for indicator
+               if (pre->l && pre->l->val == curr->val) // checks whether the found node is
+                    indiactor = 0;                     // to the left of pre/parent or not
+               else                                    // if in left set 0 of 1 indicates it
+                    indiactor = 1;                     // is in right
+
+               // only with indicator we can decide whether to delete on left or on right of pre
+
+               // the following if-else checks the nature of curr(node to be deleted)
+               // whether it is leaf node(with no child) or with left or right child
+               // or with both (where both goes in else)
+               if (!curr->l && !curr->r) // if leaf node
+               {
+                    if (indiactor)
+                    {
+                         pre->r = nullptr;
+                    }
+                    else
+                    {
+                         pre->l = nullptr;
+                    }
+                    return;
+               }
+               else if (curr->l && !curr->r) // if node with left child
+               {
+                    if (indiactor)
+                    {
+                         pre->r = curr->l;
+                    }
+                    else
+                    {
+                         pre->l = curr->l;
+                    }
+               }
+               else if (curr->r && !curr->l) // if node with right child
+               {
+                    if (indiactor)
+                    {
+                         pre->r = curr->r;
+                    }
+                    else
+                    {
+                         pre->l = curr->r;
+                    }
+               }
+               else // a sub FBT(full balanced tree)
+               {
+                    // as we know that the successive node have to be choosen from
+                    // right subtree or from left sub tree
+                    // if choosing from right then choose minimum from right sub tree
+                    // if choosing from left then choose max from left sub tree
+                    // where "getGreatest" return pre of the greatest of left sub tree
+                    // thus we have choosen left sub tree
+                    Node *t1 = getGreatest(curr->l);
+                    // if pre don't exist
+                    // then curr in pre
+                    if (!t1)
+                    {
+                         // as no pre is found curr in pre and only one value exist in it left sub tree
+                         // thus replace curr value with is left node's value
+                         // then set curr's left to null
+                         curr->val = curr->l->val;
+                         curr->l = nullptr;
+                         return;
+                    }
+                    // else change curr to pre's greatest which is at right
+                    // then set pre's right to null
+                    curr->val = t1->r->val;
+                    t1->r = nullptr;
+               }
+               return;
+          }
+          // if no node is found
+          cout
+              << "Value not found" << endl;
+     }
+};
+
+int main()
+{
+     Tree t;
+     t.add(100);
+     t.add(90);
+     t.add(110);
+     t.add(95);
+     t.add(93);
+     t.add(80);
+     t.add(97);
+     t.add(102);
+     t.add(115);
+     t.print();
+     t.del(90);
+     cout << endl
+          << "After delete" << endl;
+     t.print();
+}
